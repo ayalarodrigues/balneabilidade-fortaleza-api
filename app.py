0b6f537c1d49ce278a0524113c7d854f0eb4d0c9
Ayala <ayalafreire7@gmail.com>
2025-08-26 20:38:27 -0300

    feat: adiciona rota /praia/<int:id> para filtrar uma praia pelo id e uma rota /praia/data/<data> para buscar uma praia em uma data específica

--- app.py
+++ app.py
@@ -13,6 +13,16 @@ praia = [
         'periodo': "11/08/2025 a 17/08/2025",
         'tipos_amostragem': "Aguas procedentes das praias",
         'data_extracao': "2025-08-14"
+    },
+
+    {
+        'id': 2,
+        'nome': "P de Iracema",
+        'status': "I",
+        'zona': "Centro",
+        'periodo': "11/08/2025 a 17/08/2025",
+        'tipos_amostragem': "Águas procedentes das praias",
+        'data_extracao': "2025-08-14"
     }
 ]
 
@@ -21,12 +31,13 @@ praia = [
 #GET /praias/zona/<nome_zona>
 @app.route('/') #a função home será executada quando a raíz for chamada
 def home():
-    return "Hello World!"
+    return "API de Balneabilidade - Fortaleza"
 
 @app.route('/praia')
 def get_praia():
     return jsonify(praia)
 
+# Filtra praias pelo nome
 @app.route('/praia/<nome>')
 def get_praia_by_name(nome):
     nome = nome.replace("-", " ")  # transforma P-do-Futuro em P do Futuro
@@ -36,6 +47,21 @@ def get_praia_by_name(nome):
     return jsonify({'message': f'Praia {nome} não encontrada'})
 
 
+# Filtra uma praia pelo id
+@app.route('/praia/<int:id>')
+def get_praia_by_id(id):
+    for i in praia:
+        if i['id'] == id:
+            return jsonify(i)
+    return jsonify({'message': f'Id {nome} não encontrado'})
 
+# Busca uma praia em uma data específica
+@app.route('/praia/data/<data>')
+def get_praia_por_data(data):
+    resultado = [p for p in praia if p['data_extracao'] == data]
+    if not resultado:
+        return jsonify({'message': f'Nenhuma praia encontrada na data {data}'})
+    return jsonify(resultado)
+    
 
 app.run(port=5000)
