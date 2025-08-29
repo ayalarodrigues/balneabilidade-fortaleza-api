b4d2724756395553c96b16943b636001c8eaa259
Ayala <ayalafreire7@gmail.com>
2025-08-28 21:14:42 -0300

    feat: acessar página da Sema e listar links dos boletins

--- scraper.py
+++ scraper.py
@@ -1,1 +1,9 @@
-print("Script de scraping iniciado com sucesso!")
+import requests
+from bs4 import BeautifulSoup
+
+url_base = "https://www.semace.ce.gov.br/boletim-de-balneabilidade/"
+res = requests.get(url_base)
+soup = BeautifulSoup(res.text, "html.parser")
+
+links = [a['href'] for a in soup.find_all('a', href=True)]
+print("Total de links encontrados:", len(links))
