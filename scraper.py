7c238ebc29ece5bcdf046ac1ba34cd2a7ac8d297
Ayala <ayalafreire7@gmail.com>
2025-08-28 21:21:39 -0300

    feat: localizar o link que corresponde ao boletim mais recente

--- scraper.py
+++ scraper.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
+# --- Baixar o boletim mais recente da Sema ---
 url_base = "https://www.semace.ce.gov.br/boletim-de-balneabilidade/"
 res = requests.get(url_base)
 soup = BeautifulSoup(res.text, "html.parser")
 
-links = [a['href'] for a in soup.find_all('a', href=True)]
-print("Total de links encontrados:", len(links))
+links_boletim = [
+    a['href'] for a in soup.find_all('a', href=True) #percorre todas as tags <a> que possuem atributo href e guarda apenas o valor do link (URL)
+    if "Boletim das Praias de Fortaleza" in a.get_text() #link do boletim correspondente á cidade de Fortaleza
+]
+
+if not links_boletim:
+    raise ValueError("Nenhum boletim encontrado.")
+
+ultimo_boletim_url = urljoin(url_base, links_boletim[0])
+print("Último boletim:", ultimo_boletim_url)
